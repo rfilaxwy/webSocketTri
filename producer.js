952f2346de6b45bc3efa269279a4e61439cebd69
Ryan <ryanfilaxwylie@gmail.com>
2019-09-04 03:36:44 -0600

    fixed port

--- producer.js
+++ producer.js
@@ -1,7 +1,7 @@
 var zmq     =   require('zeromq'),
     sock    =   zmq.socket('push');
 
-sock.bindSync('tcp://127.0.0.1:300');
+sock.bindSync('tcp://127.0.0.1:3000');
 console.log('Producer bound to port 3000');
 
 setInterval(function() {
